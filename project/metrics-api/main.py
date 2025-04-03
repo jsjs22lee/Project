4240a20158c9c1025ad6264e501a517e3e5ba66c
jsjs22lee <jsjs22.lee@gmail.com>
2025-04-03 18:26:23 +0900

    Update main.py

--- project/metrics-api/main.py
+++ project/metrics-api/main.py
@@ -1,15 +1,12 @@
 from flask import Flask, jsonify
-from scheduler import get_best_node
-
+from scheduler import generate_dynamic_config
+import os
 app = Flask(__name__)
-
-@app.route("/best-node")
-def best_node():
-    try:
-        node = get_best_node()
-        return jsonify({"node": node})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+@app.route("/update")
+def update_config():
+    config = generate_dynamic_config()
+    with open("/traefik/dynamic_conf.yml", "w") as f:
+        f.write(config)
+    return jsonify({"status": "updated"})
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
